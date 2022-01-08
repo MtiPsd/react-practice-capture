6d9a9718b9577d64e2db21ac4a39aebe8e9a04d8
MtiPsd <mtipsd.git@gmail.com>
2022-01-08 21:53:42 +0330

    2. About Section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import home1 from "../img/home1.jpg";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="discription">
+        {/* title */}
+        <div className="title">
+          <div className="hide">
+            <h2>ما در تلاشیم تا</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              <span>رویاهایتان</span> را
+            </h2>
+          </div>
+          <div className="hide">
+            <h2> حقیقت ببخشیم</h2>
+          </div>
+        </div>
+        {/* title end */}
+        <p>
+          با ما در تماس باشید تا با به روز ترین و حرفه ای ترین ایده های طراحی
+          روز دنیا آشنا شوید
+        </p>
+        <button>تماس با ما</button>
+      </div>
+      {/* About image */}
+      <div className="image">
+        <img src={home1} alt="landing picture" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
